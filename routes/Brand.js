956b5eebf99c637a0391aa990da9f49aed36937b
Sonu Kumar Sharma <151660446+SonuShar4a@users.noreply.github.com>
2025-03-15 21:02:26 +0530

    Delete routes directory

--- routes/Brand.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const { fetchBrand, createBrand } = require('../controller/Brand');
-
-const router=express.Router();
-
-router.get('/',fetchBrand)
-      .post('/',createBrand)
-
-exports.router=router;
